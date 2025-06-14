27542d8fd9c32e2c1992038eb0293db035c60701
thehipnotist <54432542+thehipnotist@users.noreply.github.com>
2025-06-14 09:47:22 +0100

    Add 'CanReply' to the database for messages

---
+++ Data/Migrations/20250614084525_MessagesCanReply.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessagesCanReply : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanReply",
+                table: "Santa_Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanReply",
+                table: "Santa_Messages");
+        }
+    }
+}
