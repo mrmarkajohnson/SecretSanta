daea585fa5d0207d23b3d22b95bd6718da3b8ed9
mrmarkajohnson <mrmarkajohnson@yahoo.co.uk>
2025-11-02 12:26:51 +0000

    Improve date display

--- Global/Extensions/System/DateExtensions.cs
+++ Global/Extensions/System/DateExtensions.cs
@@ -1,9 +1,16 @@
-﻿namespace Global.Extensions.System;
+﻿using System.Globalization;
+
+namespace Global.Extensions.System;
 
 public static class DateExtensions
 {
     public static string ToDateTimeString(this DateTime date)
     {
-        return date.ToLongDateString() + " " + date.ToString("HH:mm");
+        return date.Dayth() + " " + date.ToString("MMM yyyy HH:mm", CultureInfo.InvariantCulture);
+    }
+
+    public static string Dayth(this DateTime date)
+    {
+        return date.Day.ToString() + date.Day.Ordinal();
     }
 }
--- Global/Extensions/System/NumberExtensions.cs
+++ Global/Extensions/System/NumberExtensions.cs
@@ -11,4 +11,21 @@ public static class NumberExtensions
     {
         return value == null || value == 0;
     }
+
+    public static string Ordinal(this int value)
+    {
+        return value switch
+        {
+            11 => "th",
+            12 => "th",
+            13 => "th",
+            _ => (Math.Abs(value) % 10) switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            }
+        };
+    }
 }
